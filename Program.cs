d0754483226000a1d0ad43e25f13a9dfa9a6ee6c
Craig Eaton <craigea92@gmail.com>
2024-04-03 13:32:19 +0100

    setup simple rockpaperscissors class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class RockPaperScissors {
+
+  private int playerScore;
+  private int enemyScore;
+
+  public RockPaperScissors() {
+    playerScore = 0;
+    enemyScore = 0;
+    
+    Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+  }
+
+  public void StartGame() {
+    
+  }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RockPaperScissors game = new();
+        game.StartGame();
+    }
+}
--- obj/Debug/net8.0/rockpaperscissors.AssemblyInfo.cs
+++ obj/Debug/net8.0/rockpaperscissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rockpaperscissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cccf32a359fd209afab71dfaa509c305c5e8dac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("rockpaperscissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rockpaperscissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
